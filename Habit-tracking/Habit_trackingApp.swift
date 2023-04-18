b4891e27d346b3d0f748f9a9ab4c356d426ef3be
carolineh-csuf <carolineh@csu.fullerton.edu>
2023-04-17 19:30:24 -0700

    Initial Commit

---
+++ Habit-tracking/Habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackingApp.swift
+//  Habit-tracking
+//
+//  Created by csuftitan on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
